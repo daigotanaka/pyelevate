de7b0ba07046fa01b00c53149984836646b0928e
daigotanaka <daigo.tanaka@gmail.com>
2014-01-06 11:26:43 -0800

    Added more details on docstring

--- elevate.py
+++ elevate.py
@@ -5,8 +5,14 @@ from win32con import SW_HIDE
 
 def elevate(command, param=""):
     """
-    Execute a shell command with an elevated rights
-    It pops User Access Control dialog to confirm the elevation
+    Execut a she shell command with elevated rights when the current
+    user can have the privilge.
+
+    Deisgned to be used with Windows Vista, 7 and 8.
+    It also works on Windows XP although it is not necessary on many
+    occasions.
+
+    It may pops User Access Control dialog to confirm the elevation.
     """
     ret = shell.ShellExecuteEx(
         hwnd=None,
